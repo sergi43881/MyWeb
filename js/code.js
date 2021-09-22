949f4234511c2a9285eeac16fa8bdaae592df011
sergi tomas <sergi.tomas@offing.es>
2021-09-22 13:08:06 +0200

    myweb 6.0

--- js/code.js
+++ js/code.js
@@ -2,8 +2,85 @@
     Validacion formulario.
 */
 window.onload = function() {
+    checkCookie();
     esborra();
+    initialize();
+    google.maps.event.addDomListener(window, "load", initialize);
 };
+
+// Cookies
+function checkCookie() {
+    let visita = getCookie("visitado");
+    if (visita == "") {
+        $("#window-notice").addClass("window-notice"); 
+        $("#entro").show();
+    }
+    else {
+        $("#window-notice").hide(); 
+        let logueado = getCookie("logueado");
+        if (logueado == "si")
+            $("#salgo").show();
+        else {
+            $("#entro").show();
+        }    
+    }   
+}
+function checkLogin() {
+    let visita = getCookie("visitado");
+    if (visita == "") {
+        $("#window-notice").addClass("window-notice"); 
+    }
+}
+function veurecookies() {
+    visita = "si";
+    setCookie("visitado", visita, 20);
+    setCookie("logueado", "no", 20);
+    $("#window-notice").hide(); 
+}
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+// Ubicacion
+var mapa;
+function initialize() {
+		// las coordenadas de Barcelona		
+		var ubicacion = new google.maps.LatLng(41.216488, 1.727132)
+	
+		var opcionesDeMapa = {
+				zoom: 17    ,
+				center: ubicacion,
+				mapTypeId: google.maps.MapTypeId.ROADMAP //SATELITE, HYBRID, ROADMAP, TERRAIN
+		};
+		// instancia un nuevo objeto Map
+		mapa = new google.maps.Map(document.getElementById("mapa"), opcionesDeMapa);										
+		// instancia un nuevo marcador ( la chincheta )
+		var bandera = "images/bandera1.png";
+		var marker = new google.maps.Marker({
+				position: ubicacion,
+				map: mapa,
+				title: "Offing Web Solutions",
+				icon: bandera
+		});		
+}
 
 // Sistema de validacion grupal. Valida campos formulario obligatorios que no esten vacios.
 
